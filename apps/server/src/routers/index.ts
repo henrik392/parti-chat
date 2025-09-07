db16fe92946f8afa7a67859546e0607300534036
Henrik Kvamme <henrik.halvorsen.kvamme@gmail.com>
2025-09-07 17:34:53 +0800

    Remove old route

--- apps/server/src/routers/index.ts
+++ apps/server/src/routers/index.ts
@@ -1,14 +1,12 @@
 import type { RouterClient } from '@orpc/server';
 import { publicProcedure } from '../lib/orpc';
 import { chatRouter } from './chat';
-import { partiesRouter } from './parties';
 
 export const appRouter = {
   healthCheck: publicProcedure.handler(() => {
     return 'OK';
   }),
   ...chatRouter,
-  ...partiesRouter,
 };
 
 export type AppRouter = typeof appRouter;
--- apps/server/src/routers/parties.ts
+++
@@ -1,2 +0,0 @@
-// Parties router - currently empty as we don't need party endpoints
-export const partiesRouter = {};
